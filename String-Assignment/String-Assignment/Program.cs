c8e44c8760e2e22d02220c2fb020ef7096e60c18
DannyF74 <Dannytfirth@outlook.com>
2024-08-10 03:41:42 -0700

    Created String Assignment

---
+++ String-Assignment/String-Assignment/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+
+namespace String_Assignment
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            // Creating three strings and concatenating them together
+            string hello = "Hello,";
+            string space = " ";
+            string world = "World!";
+            Console.WriteLine(hello + space + world);
+            Console.ReadLine();
+
+            // Writes the world string in upper case
+            Console.WriteLine(world.ToUpper());
+            Console.ReadLine();
+
+            // Creating a StringBuilder to show how to add lines of text to a string.
+            StringBuilder sb = new StringBuilder();
+            sb.Append("This is a new string builder.\n");
+            sb.Append("This will build a paragraph of text\n");
+            sb.Append("as one string\n");
+            sb.Append("using multiple appends.");
+            Console.WriteLine(sb.ToString());
+            Console.ReadLine();
+        }
+    }
+}
