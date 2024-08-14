84a89ffa5879068edf8e0005883adcccf46fb8fb
DannyF74 <Dannytfirth@outlook.com>
2024-08-14 14:31:12 -0700

    Created Void Method Assignment

---
+++ Void-Method-Assignment/Void-Method-Assignment/MathOperations.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Void_Method_Assignment
+{
+    public class MathOperations
+    {
+        // Here we are creating a void method which does not return any values but prints to the console
+        // from within the method. We are performing a calculation on only one of the parameters. We print
+        // the result of that calculation to the console and print the other untouched parameter to the console.
+        public void calc1(int x, int y)
+        {
+            int result = x + 10;
+
+            Console.WriteLine("10 plus " + x + " is " + result);
+            Console.WriteLine(y + " is still " + y);
+        }
+    }
+}
---
+++ Void-Method-Assignment/Void-Method-Assignment/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Void_Method_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiating our class and asking for user input
+            MathOperations mathOps = new MathOperations();
+            Console.WriteLine("Please enter a number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter another number");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            // Running our method with implicit parameters
+            Console.WriteLine("\nCalculating...");
+            mathOps.calc1(num1, num2);
+            Console.WriteLine();
+
+            // Running our method with explicit parameters.
+            Console.WriteLine("Once again but specifically naming the parameters...");
+            mathOps.calc1(x: num1, y: num2);
+            Console.ReadLine();
+        }
+    }
+}
