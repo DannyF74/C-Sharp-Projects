90bc477382ffd727568cff187ddc0e94b606d107
DannyF74 <Dannytfirth@outlook.com>
2024-08-13 17:45:38 -0700

    Created Calling Methods Assignment

---
+++ Calling-Methods-Assignment/Calling-Methods-Assignment/Methods.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calling_Methods_Assignment
+{
+    // Creating the class and making it public so it can be accessed from anywhere in the namespace
+    public class mathOperations
+    {
+        // Creating new public methods
+        public int addTen(int number)
+        {
+            return number + 10;
+        }
+
+        public int multiplyTwo(int number)
+        { 
+            return number * 2; 
+        }
+
+        public int multiplyTen(int number)
+        {
+            return number * 10;
+        }
+    }
+}
---
+++ Calling-Methods-Assignment/Calling-Methods-Assignment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calling_Methods_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiating the class with our math methods inside of it from the methods.cs file.
+            mathOperations mathOps = new mathOperations();
+
+            // Asking the user for input
+            Console.WriteLine("Please enter the number you'd like to perform some math operations on.");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            // Calling each method from the created class
+            Console.WriteLine(number + " plus 10 equals " + mathOps.addTen(number));
+            Console.WriteLine(number + " multiplied by 2 equals " + mathOps.multiplyTwo(number));
+            Console.WriteLine(number + " multiplied by 10 equals " + mathOps.multiplyTen(number));
+            Console.ReadLine();
+        }
+    }
+}
