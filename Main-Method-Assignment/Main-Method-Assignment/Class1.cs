a904d6ca438a1adefe7813fccadaa76c3881c33b
DannyF74 <Dannytfirth@outlook.com>
2024-08-14 11:53:29 -0700

    Created Main Method Assignment

---
+++ Main-Method-Assignment/Main-Method-Assignment/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Method_Assignment
+{
+    public class Class1
+    {
+        // Creating new overloaded method called Calculation. This will give a different answer
+        // depending on what data type it is being given.
+        public int Calculation(int number1)
+        {
+            return number1 + 1;
+        }
+
+        public decimal Calculation(decimal number2)
+        {
+            return number2 + 10;
+        }
+
+        public string Calculation(string stringNumber) 
+        {
+            int intNumber = Convert.ToInt32(stringNumber);
+            intNumber = intNumber + 100;
+            stringNumber = Convert.ToString(intNumber);
+            return stringNumber;
+        }
+    }
+}
---
+++ Main-Method-Assignment/Main-Method-Assignment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Method_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiating the class we just made and named it Calculations.
+            Class1 Calculations = new Class1();
+            Console.WriteLine("Please enter your favorite number.");
+            string userNumber = Console.ReadLine();
+
+            // Takes the users input and runs the overloaded method while converting the input to different data types.
+            // Although everything looks the same, the different data types will run different methods and produce
+            // different answers.
+            Console.WriteLine(userNumber + " plus one as an integer equals " + Calculations.Calculation(Convert.ToInt32(userNumber)));
+            Console.WriteLine(userNumber + " plus ten as a decimal equals " + Calculations.Calculation(Convert.ToDecimal(userNumber)));
+            Console.WriteLine(userNumber + " plus one hundred as a string equals " + Calculations.Calculation(userNumber));
+            Console.ReadLine();
+        }
+    }
+}
