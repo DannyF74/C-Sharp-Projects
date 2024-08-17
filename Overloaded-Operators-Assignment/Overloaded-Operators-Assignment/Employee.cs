48991647fc58c1ccd72bd90500552d0ea3afc145
DannyF74 <Dannytfirth@outlook.com>
2024-08-16 23:36:51 -0700

    Created Overloaded Operators Assignment

---
+++ Overloaded-Operators-Assignment/Overloaded-Operators-Assignment/Employee.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overloaded_Operators_Assignment
+{
+    public class Employee
+    {
+        // Creating properties for the Employees
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public int ID { get; set; }
+
+        // Overriding the == operator to run a check on if employee IDs match
+        public static bool operator ==(Employee employee1, Employee employee2)
+        {
+            Console.WriteLine("Do the IDs of these employees match?");
+            if (employee1.ID == employee2.ID)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        // Overriding the != to run a check on if employee IDs are different.
+        public static bool operator !=(Employee employee1, Employee employee2)
+        {
+            Console.WriteLine("Are the IDs of the employees different?");
+            if (employee1.ID != employee2.ID)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Overloaded-Operators-Assignment/Overloaded-Operators-Assignment/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overloaded_Operators_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiating two new employees
+            Employee employee1 = new Employee();
+            Employee employee2 = new Employee();
+
+            // Assigning values to each objects properties.
+            employee1.ID = 1;
+            employee1.firstName = "Danny";
+            employee1.lastName = "Firth";
+
+            employee2.ID = 2;
+            employee2.firstName = "Test";
+            employee2.lastName = "Subject";
+
+            // Displaying the values of the objects to the user of the program.
+            Console.WriteLine("Employee ID: " + employee1.ID + "\nFirst name: " + employee1.firstName + 
+                "\nLast name: " + employee1.lastName);
+            Console.WriteLine("\nEmployee ID: " + employee2.ID + "\nFirst name: " + employee2.firstName +
+                "\nLast name: " + employee2.lastName);
+
+            // Running the overloaded operators that we defined in our Employee class.
+            Console.WriteLine(employee1 == employee2);
+            Console.WriteLine(employee1 != employee2);
+            Console.ReadLine(); 
+        }
+    }
+}
