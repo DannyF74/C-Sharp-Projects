ab12a41743a8ed6cdd537970dd3c2f3457aa2df5
DannyF74 <Dannytfirth@outlook.com>
2024-09-09 18:35:21 -0700

    Created CodeFirst Project

---
+++ CodeFirst/CodeFirst/DAL/StudentContext.cs
@@ -0,0 +1,18 @@
+﻿using CodeFirst.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CodeFirst.DAL
+{
+    public class StudentContext : DbContext
+    {
+        public StudentContext() : base("StudentContext")
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ CodeFirst/CodeFirst/DAL/StudentInitializer.cs
@@ -0,0 +1,22 @@
+﻿using CodeFirst.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CodeFirst.DAL
+{
+    public class StudentInitializer : System.Data.Entity.DropCreateDatabaseIfModelChanges<StudentContext>
+    {
+        protected override void Seed(StudentContext context)
+        {
+            var students = new List<Student>
+            {
+            new Student{FirstName="Danny", LastName="Firth", Age=27}
+            };
+
+            students.ForEach(s => context.Students.Add(s));
+            context.SaveChanges();
+        }
+    }
+}
